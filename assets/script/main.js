bc912a85140504d683f92859bd09cf5d6f65f169
huseynahmadov <t.huseyn96@mail.ru>
2021-12-25 01:37:18 +0400

    fixed bugs

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 
 import {
-  getDatabase,
-  ref,
-  set,
-  onValue,
-  push,
+    getDatabase,
+    ref,
+    set,
+    onValue,
+    push,
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 import {
-  getAuth,
-  signOut,
-  signInWithEmailAndPassword,
+    getAuth,
+    signOut,
+    signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
-  authDomain: "book-store-69694.firebaseapp.com",
-  projectId: "book-store-69694",
-  storageBucket: "book-store-69694.appspot.com",
-  messagingSenderId: "434964076450",
-  appId: "1:434964076450:web:40c86964585bb16b8384fb",
+    apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
+    authDomain: "book-store-69694.firebaseapp.com",
+    projectId: "book-store-69694",
+    storageBucket: "book-store-69694.appspot.com",
+    messagingSenderId: "434964076450",
+    appId: "1:434964076450:web:40c86964585bb16b8384fb",
 });
 
 // ==============
@@ -29,99 +29,99 @@ const firebaseApp = initializeApp({
 let userId = 1;
 
 const validateEmail = (email) => {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 };
 
 const auth = getAuth(firebaseApp);
 var join = $(".join");
-join.on("click", function (e) {
-  const email = $(".username").val();
-  const password = $(".password").val();
+join.on("click", function(e) {
+    const email = $(".username").val();
+    const password = $(".password").val();
 
-  var MinLength = 6;
-  var MaxLength = 15;
-  if (password.length < MinLength || password.length > MaxLength) {
-    $("#password-error-message").text("Şifrə 6-12 aralığı olmadır");
-    $("#password-error-message").css({
-      color: "red",
-    });
-  }
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      console.log("user logged in:", userCredential.user);
-
-      document.querySelector(".login-container").classList.add("d-none");
-      document.querySelector(".myGif").src =
-        "assets/images/Infinity-1s-200px.gif";
-
-      setTimeout(function () {
-        document.querySelector(".myGif").src = "";
-        window.location = "./admin.html";
-      }, 3000);
-    })
-    .catch((error) => {
-      console.log(error.message);
-      //  Email validate
-      const $result = $("#email-error-message");
-      const email = $(".username").val();
-      $result.text("");
-
-      if (!validateEmail(email)) {
-        $result.text("düzgün email adresi daxil edin");
-        $result.css({
-          color: "red",
-          "font-size": "20px",
-          "font-weight": "400",
+    var MinLength = 6;
+    var MaxLength = 15;
+    if (password.length < MinLength || password.length > MaxLength) {
+        $("#password-error-message").text("Şifrə 6-12 aralığı olmadır");
+        $("#password-error-message").css({
+            color: "red",
         });
-        return false;
-      }
-    });
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log("user logged in:", userCredential.user);
+
+            document.querySelector(".login-container").classList.add("d-none");
+            document.querySelector(".myGif").src =
+                "assets/images/Infinity-1s-200px.gif";
+
+            setTimeout(function() {
+                document.querySelector(".myGif").src = "";
+                window.location = "./admin.html";
+            }, 3000);
+        })
+        .catch((error) => {
+            console.log(error.message);
+            //  Email validate
+            const $result = $("#email-error-message");
+            const email = $(".username").val();
+            $result.text("");
+
+            if (!validateEmail(email)) {
+                $result.text("düzgün email adresi daxil edin");
+                $result.css({
+                    color: "red",
+                    "font-size": "20px",
+                    "font-weight": "400",
+                });
+                return false;
+            }
+        });
 });
 
 // ==============
 // SIGN OUT ADMIN PANEL
 // ==============
-$(document).on("click", "#log-out", function () {
-  console.log("asd");
-  signOut(auth)
-    .then((userCredential) => {
-      window.location = "./admin-login.html";
-      console.log("userCredential");
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+$(document).on("click", "#log-out", function() {
+    console.log("asd");
+    signOut(auth)
+        .then((userCredential) => {
+            window.location = "./admin-login.html";
+            console.log("userCredential");
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 });
 
 const firebaseConfig = initializeApp({
-  apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
-  authDomain: "book-store-69694.firebaseapp.com",
-  projectId: "book-store-69694",
-  storageBucket: "book-store-69694.appspot.com",
-  messagingSenderId: "434964076450",
-  appId: "1:434964076450:web:40c86964585bb16b8384fb",
+    apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
+    authDomain: "book-store-69694.firebaseapp.com",
+    projectId: "book-store-69694",
+    storageBucket: "book-store-69694.appspot.com",
+    messagingSenderId: "434964076450",
+    appId: "1:434964076450:web:40c86964585bb16b8384fb",
 });
 
 // const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
-$("#send-button").on("click", function () {
-  let name = $("#name").val();
-  let surname = $("#surname").val();
-  let email = $("#email").val();
-  let phone = $("#phone").val();
+$("#send-button").on("click", function() {
+    let name = $("#name").val();
+    let surname = $("#surname").val();
+    let email = $("#email").val();
+    let phone = $("#phone").val();
 
-  var objKey = push(ref(db, "/")).key;
+    var objKey = push(ref(db, "/")).key;
 
-  set(ref(db, "contact-us-infos/" + objKey), {
-    username: name,
-    surname: surname,
-    email,
-    phone,
-  });
+    set(ref(db, "contact-us-infos/" + objKey), {
+        username: name,
+        surname: surname,
+        email,
+        phone,
+    });
 });
 
 /* 
@@ -138,17 +138,17 @@ $("#send-button").on("click", function () {
 
 const starCountRef = ref(db, "contact-us-infos/");
 onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  let count = 0;
+    const data = snapshot.val();
+    let count = 0;
 
-  $("#contact-infos").empty();
+    $("#contact-infos").empty();
 
-  for (let result in data) {
-    count++;
+    for (let result in data) {
+        count++;
 
-    var users = data[result];
+        var users = data[result];
 
-    var newDatas = $(`<tr>
+        var newDatas = $(`<tr>
             <th scope="row">${count}</th>
             <td>${users.username}</td>
             <td>${users.surname}</td>
@@ -156,49 +156,49 @@ onValue(starCountRef, (snapshot) => {
             <td>${users.phone}</td>
             </tr>`);
 
-    $("#contact-infos").append(newDatas);
-  }
+        $("#contact-infos").append(newDatas);
+    }
 });
 
 // ==============
 // WRITE JOIN US INFOS FROM FIREBASE TO ADMIN PANEL
 // ==============
 
-$("#join-us").on("click", function () {
-  let name = $("#modal-firstname").val();
-  let surname = $("#modal-surname").val();
-  let email = $("#modal-email").val();
+$("#join-us").on("click", function() {
+    let name = $("#modal-firstname").val();
+    let surname = $("#modal-surname").val();
+    let email = $("#modal-email").val();
 
-  var objKey = push(ref(db, "/")).key;
-  console.log(objKey);
-  set(ref(db, "join-us-infos/" + objKey), {
-    username: name,
-    surname,
-    email,
-  });
+    var objKey = push(ref(db, "/")).key;
+    console.log(objKey);
+    set(ref(db, "join-us-infos/" + objKey), {
+        username: name,
+        surname,
+        email,
+    });
 });
 
 const usersInfos = ref(db, "join-us-infos/");
 onValue(usersInfos, (snapshot) => {
-  const data = snapshot.val();
-  let count = 0;
+    const data = snapshot.val();
+    let count = 0;
 
-  $("#join-us-infos").empty();
+    $("#join-us-infos").empty();
 
-  for (let result in data) {
-    count++;
+    for (let result in data) {
+        count++;
 
-    var users = data[result];
+        var users = data[result];
 
-    var newDatas = $(`<tr>
+        var newDatas = $(`<tr>
             <th scope="row">${count}</th>
             <td>${users.username}</td>
             <td>${users.surname}</td>
             <td>${users.email}</td>
             </tr>`);
 
-    $("#join-us-infos").append(newDatas);
-  }
+        $("#join-us-infos").append(newDatas);
+    }
 });
 /* 
 
