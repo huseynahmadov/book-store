a8a11dbcfe7d3956f4a8110e0946aab21ad65602
huseynahmadov <t.huseyn96@mail.ru>
2021-12-25 21:03:52 +0400

    fixing bugs

--- assets/script/slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.your-class').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-});
