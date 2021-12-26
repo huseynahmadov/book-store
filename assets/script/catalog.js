9966531926a44de6abfc7c3d232e5729d6429bb0
thehasanovv <hasanov.e95@gmail.com>
2021-12-27 01:48:34 +0400

    catalog.js

--- assets/script/catalog.js
+++ assets/script/catalog.js
@@ -1,116 +1,30 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import {
-    getDatabase,
-    ref,
-    set,
-    onValue,
-    push,
+  getDatabase,
+  ref,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-import {
-    getAuth,
-    signOut,
-    signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-
 const firebaseConfig = initializeApp({
-    apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
-    authDomain: "book-store-69694.firebaseapp.com",
-    projectId: "book-store-69694",
-    storageBucket: "book-store-69694.appspot.com",
-    messagingSenderId: "434964076450",
-    appId: "1:434964076450:web:40c86964585bb16b8384fb",
+  apiKey: "AIzaSyAJ-eqplSjwcTbbHbewQzlUe9Y8otdbYto",
+  authDomain: "book-store-69694.firebaseapp.com",
+  projectId: "book-store-69694",
+  storageBucket: "book-store-69694.appspot.com",
+  messagingSenderId: "434964076450",
+  appId: "1:434964076450:web:40c86964585bb16b8384fb",
 });
-
 
 const db = getDatabase();
 
-window.onload = booksLoad;
+const booksInfo = ref(db, "newBooks/");
+let clearAllBook = function (emptyDiv, bookType) {
+  onValue(booksInfo, (snapshot) => {
+    const data = snapshot.val();
 
-function booksLoad() {
-    const booksInfo = ref(db, "newBooks/");
-    onValue(booksInfo, (snapshot) => {
-        const data = snapshot.val();
-
-
-        for (let result in data) {
-
-            let books = data[result];
-
-            let newBook = $(`<div>
-            <div class="card " style="width: 15rem; ">
-                <img class="card-img-top " src="${books.imageUrl}" alt="Card image cap " style="height: 200px; ">
-                <div class="card-body ">
-                    <h5 class="card-title text-center ">${books.bookName}</h5>
-                    <p class="card-text text-center ">${books.authorName}</p>
-                    <a href="# " class="btn btn-primary " style="margin-left: 22% !important ">Read more</a>
-                </div>
-            </div>
-        </div>`);
-            $('.items').append(newBook)
-
-
-        }
-
-        $('.asass').html('<script src="./assets/script/slider.js " defer></script>')
-    });
-}
-
-$('.fantastic').on('click', function() {
-
-    $(".fantastic").off('click');
-    $("#adventures").on('click');
-    $('.items').css("display", "none");
-    $('#adventure').css("display", "none")
-    $('.fantastics').css("display", "block")
-    const booksInfo = ref(db, "newBooks/");
-    onValue(booksInfo, (snapshot) => {
-        const data = snapshot.val();
-
-
-        for (let result in data) {
-            var books = data[result];
-            if (books.bookType === 'Fantastic') {
-
-
-                let newBook = $(`<div>
-            <div class="card " style="width: 15rem; ">
-                <img class="card-img-top " src="${books.imageUrl}" alt="Card image cap " style="height: 200px; ">
-                <div class="card-body ">
-                    <h5 class="card-title text-center ">${books.bookName}</h5>
-                    <p class="card-text text-center ">${books.authorName}</p>
-                    <a href="# " class="btn btn-primary " style="margin-left: 22% !important ">Read more</a>
-                </div>
-            </div>
-        </div>`);
-
-                $('.fantastics').append(newBook)
-            }
-
-        }
-
-        $('.asass').html('<script src="./assets/script/slider2.js " defer></script>')
-    });
-
-})
-$("#adventures").on('click', function() {
-    $("#adventures").off('click');
-    $(".fantastic").on('click');
-    $(".fantastics").css("display", "none");
-    $('.items').css("display", "none");
-    $('#adventure').css("display", "block")
-
-    const booksInfo = ref(db, "newBooks/");
-    onValue(booksInfo, (snapshot) => {
-        const data = snapshot.val();
-
-
-        for (let result in data) {
-            var books = data[result];
-            if (books.bookType === 'Adventure') {
-
-
-                let newBook = $(`<div>
+    for (let result in data) {
+      var books = data[result];
+      if (books.bookType === `${bookType}`) {
+        let newBook = $(`<div>
             <div class="card " style="width: 15rem; ">
                 <img class="card-img-top " src="${books.imageUrl}" alt="Card image cap " style="height: 200px; ">
                 <div class="card-body ">
@@ -121,13 +35,71 @@ $("#adventures").on('click', function() {
             </div>
         </div>`);
 
-                $('#adventure').append(newBook)
-            }
-        }
-        $('.asass').html('<script src="./assets/script/slider3.js " defer></script>')
+        $(emptyDiv).append(newBook);
+      }
+    }
+
+    $(document).ready(function () {
+      $(emptyDiv).not(".slick-initialized").slick({
+        infinite: true,
+        slidesToShow: 3,
+        lazyLoad: "ondemand",
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+      });
     });
-})
+  });
+};
+var myArray = [];
 
+let uniqueBookType = function (booksTypes) {
+  $.each(booksTypes, function (index, bookType) {
+    clearAllBook("#bestseller", bookType);
+    clearAllBook("#all-books-slick", bookType);
+  });
+};
 
+let allBookstypes = function () {
+  onValue(booksInfo, (snapshot) => {
+    const data = snapshot.val();
+    for (let booktype in data) {
+      var result = data[booktype];
+      if (!myArray.includes(result.bookType)) {
+        myArray.push(result.bookType);
+      }
+    }
+    uniqueBookType(myArray);
+  });
+};
 
-console.log('salam')
+allBookstypes();
+clearAllBook("#fantastic-slick", "Fantastic");
+clearAllBook("#crime-slick", "Crime");
+clearAllBook("#detective-slick", "Detective");
+clearAllBook("#adventures-slick", "Adventure");
+
+let hideCategories = function () {
+  $("#all-categories").children().addClass("d-none");
+};
+
+$("#crime").on("click", function () {
+  hideCategories();
+  $("#crime-slick").removeClass("d-none");
+});
+
+$("#detective").on("click", function () {
+  hideCategories();
+  $("#detective-slick").removeClass("d-none");
+});
+
+$("#adventures").on("click", function () {
+  hideCategories();
+  $("#adventures-slick").removeClass("d-none");
+});
+
+$("#fantastic").on("click", function () {
+  hideCategories();
+  $("#fantastic-slick").removeClass("d-none");
+});
