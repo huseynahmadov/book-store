d5beadd19db4d1d9edd45ec99178f04df7f6a4c9
huseynahmadov <t.huseyn96@mail.ru>
2021-12-26 20:06:01 +0400

    catalog section updated

---
+++ assets/script/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        lazyLoad: 'ondemand',
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true
+    });
+
+
+
+});
+console.log('salammgisfdsfsf')
---
+++ assets/script/slider2.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    
+    $('.fantastics').slick({
+        infinite: true,
+        slidesToShow: 3,
+        lazyLoad: 'ondemand',
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true
+    });
+
+
+});
